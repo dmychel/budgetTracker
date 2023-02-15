0ef9c9f8f30b9e933c565aaf7798043c649e7071
noodles <dmychel@tutanota.com>
2023-02-15 03:39:13 +0000

    sum function
    addded a const variable that takes the 'number' variable and make a an number rather than a string. I then made  function that takes the numebr and addes a fixed number to it. The result is displayed on the webpage next to the 'Total' row.

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,33 @@
 console.log('hello world')
 
 // Table variables to grab users entries
+
+// Date
 const date = document.getElementById('date').value;
+
+// Item Description
 const description = document.getElementById('description').value;
+
+// Expense or Income
 const numberType = document.getElementById('numberType').value;
+
+// Amount Row
 const number = document.getElementById('number').value
+const stringToNum = Number(number);
 
 const total = document.getElementById('total')
-total.innerHTML = 'Hello World'
 
 
-console.log(date)
-console.log(description)
-console.log(numberType)
-console.log(number)
+
+function sum (){
+    let totalCash = stringToNum + 200;
+    total.innerHTML = totalCash
+}
+
+sum()
+// console.log(sum())
+
+// console.log(date)
+// console.log(description)
+// console.log(numberType)
+// console.log(number)
