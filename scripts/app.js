873499ffa8f264a5034e5161d1bd0807cd7262d8
noodles <dmychel@tutanota.com>
2023-02-15 02:46:40 +0000

    const variables
    crated variables that grabbed the html via ID and confirmed I can retrieve user inputted data from the table

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,15 @@
 console.log('hello world')
+
+const date = document.getElementById('date').value;
+
+const description = document.getElementById('description').value;
+
+const numberType = document.getElementById('numberType').value;
+
+const number = document.getElementById('number').value
+
+
+console.log(date)
+console.log(description)
+console.log(numberType)
+console.log(number)
