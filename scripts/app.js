d4a84851a404f80a3b2b3a7ce56f64d48f5f4be6
noodles <dmychel@tutanota.com>
2023-02-15 03:01:23 +0000

    insert html
    created a const variable for span class to be enable insertion of html

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,13 @@
 console.log('hello world')
 
+// Table variables to grab users entries
 const date = document.getElementById('date').value;
-
 const description = document.getElementById('description').value;
-
 const numberType = document.getElementById('numberType').value;
-
 const number = document.getElementById('number').value
+
+const total = document.getElementById('total')
+total.innerHTML = 'Hello World'
 
 
 console.log(date)
